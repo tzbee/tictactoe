33f3412cb6a1ba1a5b42a82eeb396ec0d9b98862
Touzbi <tolbimine@gmail.com>
2015-06-11 06:37:54 +0200

    simplified game and optimized ai

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-var canvas = document.getElementById('grid');
+var canvas = document.getElementById('gridCanvas');
 var ctx = canvas.getContext('2d');
+var theButton = document.getElementById('theButton');
 
-var TicTacToe = require('./tic-tac-toe');
+theButton.addEventListener('click', start);
+canvas.addEventListener('mousedown', onClick, false);
 
 var squareSize = 100;
 
-var Ai = require('./ai.js');
+var grid = [
+	['', '', ''],
+	['', '', ''],
+	['', '', '']
+];
 
-var sides = {
-	ai: 'x',
-	player: 'o'
+var tokens = {
+	player: 'x',
+	ai: 'o'
 };
 
-var ai = new Ai(sides.ai);
-var firstTurn = sides.ai;
+var turn = 'player';
+var gameOver = true;
+var winner = '';
 
-var model = new TicTacToe(firstTurn);
-model.gameOver = true;
-
-render(ctx);
+render(grid, ctx);
 
 function start() {
-	model.gameOver = false;
+	gameOver = false;
 
-	if (isTurnOf('ai')) {
-		aiPlays();
+	if (turn === 'ai') {
+		aiPlays(grid, function() {
+			nextTurn();
+		});
 	}
 }
 
-function render(ctx) {
-	var grid = model.grid;
-
+function render(grid, ctx) {
 	// Render grid content
 
 	for (var i = 0; i < 3; i++) {
@@ -66,12 +70,26 @@ function render(ctx) {
 	ctx.stroke();
 }
 
-function isTurnOf(side) {
-	return model.turn === sides[side];
+function nextTurn() {
+	render(grid, ctx);
+	var winner = checkWinner(grid);
+
+	if (winner || isFullGrid(grid)) {
+		showWinner(winner);
+	} else {
+
+		turn = turn === 'ai' ? 'player' : 'ai';
+
+		if (turn === 'ai') {
+			aiPlays(grid, function() {
+				nextTurn();
+			});
+		}
+	}
 }
 
 function onClick(event) {
-	if (model.gameOver || !isTurnOf('player')) return;
+	if (gameOver || turn !== 'player') return;
 
 	var x = event.pageY,
 		y = event.pageX;
@@ -84,20 +102,18 @@ function onClick(event) {
 
 	gridPos = [gridRow < 0 ? 0 : gridRow > 2 ? 2 : gridRow, gridColumn < 0 ? 0 : gridColumn > 2 ? 2 : gridColumn];
 
-	var validMove = play(gridPos);
+	var validMove = play(grid, tokens.player, gridPos);
 
-	if (validMove) {
-		aiPlays();
-	}
+	if (validMove) nextTurn();
 }
 
-function aiPlays() {
-	if (model.gameOver || !isTurnOf('ai')) return;
-
-	ai.getNextMove(model, function(nextMove) {
+function aiPlays(grid, done) {
+	getNextMove(grid, function(nextMove) {
 		hideSpinner();
-		play(nextMove);
+		play(grid, tokens.ai, nextMove);
+		done();
 	});
+
 	showSpinner();
 }
 
@@ -119,33 +135,182 @@ function hideSpinner() {
 	}
 }
 
-function play(pos) {
+function showWinner(winner) {
+	var message = winner ? winner + ' wins!' : 'Draw!';
 
-	// Play
-	var validMove = model.play(pos);
+	var r = confirm(message + ', replay?');
 
-	if (!validMove) {
-		return false;
+	if (r === true) {
+		reset(grid);
+		render(grid, ctx);
+	}
+}
+
+function checkWinner(grid) {
+	var winner;
+
+	for (var i = 0; i < 3; i++) {
+		winner = checkLine(getRow(grid, i));
+		if (winner) return winner;
 	}
 
-	// Render the game
-	render(ctx);
+	for (i = 0; i < 3; i++) {
+		winner = checkLine(getColumn(grid, i));
+		if (winner) return winner;
+	}
 
-	// Check winner
-	if (model.gameOver) {
-		var winner = model.winner === 'x' ? 'red' : model.winner === 'o' ? 'blue' : '';
-		var message = winner ? winner + ' wins!' : 'Draw!';
+	winner = checkLine(getDiag1(grid));
+	if (winner) return winner;
 
-		var r = confirm(message + ', replay?');
+	winner = checkLine(getDiag2(grid));
+	if (winner) return winner;
 
-		if (r === true) {
-			model.reset();
-			render(ctx);
+	return '';
+}
+
+function play(grid, token, pos) {
+	if (isValidPosition(grid, pos)) {
+		grid[pos[0]][pos[1]] = token;
+	} else {
+		return false;
+	}
+	return true;
+}
+
+function isValidPosition(grid, pos) {
+	if (!pos || pos && pos.length !== 2) return false;
+
+	var row = grid[pos[0]];
+
+	if (row === null) return false;
+
+	var piece = row[pos[1]];
+
+	return piece === '';
+}
+
+function checkLine(line) {
+	var nbX = nbOfOccurences('x', line);
+	var nbO = nbOfOccurences('o', line);
+
+	if (nbX === 3) {
+		return 'x';
+	}
+	if (nbO === 3) {
+		return 'o';
+	} else {
+		return '';
+	}
+}
+
+function getRow(grid, rowIndex) {
+	return grid[rowIndex];
+}
+
+function getColumn(grid, colIndex) {
+	return [grid[0][colIndex], grid[1][colIndex], grid[2][colIndex]];
+}
+
+function getDiag1(grid) {
+	return [grid[0][0], grid[1][1], grid[2][2]];
+}
+
+function getDiag2(grid) {
+	return [grid[2][0], grid[1][1], grid[0][2]];
+}
+
+function nbOfOccurences(piece, array) {
+	var nbOfOcc = 0;
+
+	array.forEach(function(p) {
+		if (p === piece) {
+			nbOfOcc++;
+		}
+	});
+
+	return nbOfOcc;
+}
+
+function reset(grid) {
+	gameOver = false;
+	winner = '';
+	turn = 'player';
+
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			grid[i][j] = '';
+		}
+	}
+}
+
+function isFullGrid(grid) {
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			if (!grid[i][j]) return false;
 		}
 	}
 
 	return true;
 }
 
-document.getElementById('theButton').addEventListener('click', start);
-canvas.addEventListener('mousedown', onClick, false);
+// AI
+
+function evaluate(grid) {
+	var winner = checkWinner(grid);
+	return !winner ? 0 : winner === tokens.ai ? 10 : -10;
+}
+
+var aiChoice;
+
+function minimax(grid, maximize) {
+	if (isFullGrid(grid) || checkWinner(grid)) {
+		return evaluate(grid);
+	}
+
+	var gridCopy;
+	var scores = [];
+	var moves = [];
+
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			gridCopy = copyGrid(grid);
+			var move = [i, j];
+			var isValidMove = play(gridCopy, maximize ? tokens.ai : tokens.player, move);
+			if (isValidMove) {
+				scores.push(minimax(gridCopy, !maximize));
+				moves.push(move);
+			}
+		}
+	}
+
+	var result;
+
+	if (maximize) {
+		result = scores.reduce(function(max, score) {
+			return score > max ? score : max;
+		}, -1000);
+	} else {
+		result = scores.reduce(function(min, score) {
+			return score < min ? score : min;
+		}, 1000);
+	}
+
+	aiChoice = moves[scores.indexOf(result)];
+
+	return result;
+}
+
+function copyGrid(grid) {
+	var gridCopy = [];
+	for (var i = 0; i < 3; i++) {
+		gridCopy.push(grid[i].slice());
+	}
+	return gridCopy;
+}
+
+function getNextMove(game, done) {
+	setTimeout(function() {
+		minimax(game, true);
+		done(aiChoice);
+	}, 0);
+}
